cac7c407099b4fcfa92e21bf73ebbfe9b94e7853
Julien <julien@blanko.ca>
2018-09-21 08:43:20 -0400

    15-18

--- 016.go
+++ 016.go
@@ -1,26 +1,25 @@
 package main
 
 import (
-    "fmt"
-    "strconv"
-    "math/big"
+	"fmt"
+	"math/big"
+	"strconv"
 )
 
 func main() {
-    const expo = 1000
+	const expo = 1000
 
-    var r big.Int
-    r.Exp(big.NewInt(2), big.NewInt(expo), nil)
+	var r big.Int
+	r.Exp(big.NewInt(2), big.NewInt(expo), nil)
 
-    str := r.String()
-    sum := 0
+	str := r.String()
+	sum := 0
 
-    for i := 0; i < len(str); i++ {
-        n, _ := strconv.Atoi(string(str[i]))
-        sum += n
-    }
+	for i := 0; i < len(str); i++ {
+		n, _ := strconv.Atoi(string(str[i]))
+		sum += n
+	}
 
-    fmt.Println("== Euler project - Problem 14 ==")
-    fmt.Println("Sum of digits from 2^", expo, "is", sum)
+	fmt.Println("== Euler project - Problem 16 ==")
+	fmt.Println("Sum of digits from 2^", expo, "is", sum)
 }
-
--- 017.go
+++ 017.go
@@ -1,91 +1,90 @@
 package main
 
 import (
-    "fmt"
-    "math"
-    "strings"
-    //"strconv"
+	"fmt"
+	"math"
+	"strings"
+	//"strconv"
 )
 
 func main() {
-    const max = 1000
-    const skipConcat = false
-    const debug = false
+	const max = 1000
+	const skipConcat = false
+	const debug = false
 
-    lettersCount := 0
+	lettersCount := 0
 
-    for i := 1; i <= max; i++ {
-        s := getWordForInt(i)
+	for i := 1; i <= max; i++ {
+		s := getWordForInt(i)
 
-        if !skipConcat {
-            s = strings.Replace(s, " ", "", -1)
-            s = strings.Replace(s, "-", "", -1)
-        }
+		if !skipConcat {
+			s = strings.Replace(s, " ", "", -1)
+			s = strings.Replace(s, "-", "", -1)
+		}
 
-        if debug {
-            fmt.Println(s)
-        }
+		if debug {
+			fmt.Println(s)
+		}
 
-        lettersCount += len(s)
-    }
+		lettersCount += len(s)
+	}
 
-    fmt.Println("== Euler project - Problem 14 ==")
-    fmt.Println(lettersCount)
+	fmt.Println("== Euler project - Problem 17 ==")
+	fmt.Println(lettersCount)
 }
 
 func getWordForInt(n int) string {
-    below20 := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"}
-    tens := []string{"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"}
+	below20 := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"}
+	tens := []string{"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"}
 
-    if n < 20 {
-        return below20[n-1]
-    }
+	if n < 20 {
+		return below20[n-1]
+	}
 
-    if n < 100 {
-        tensIndex := int(math.Floor(float64(n) / 10))
-        remaining := n - tensIndex * 10
-        remainingStr := ""
+	if n < 100 {
+		tensIndex := int(math.Floor(float64(n) / 10))
+		remaining := n - tensIndex*10
+		remainingStr := ""
 
-        if remaining > 0 {
-            remainingStr = "-" + getWordForInt(remaining)
-        }
+		if remaining > 0 {
+			remainingStr = "-" + getWordForInt(remaining)
+		}
 
-        tensStr := tens[tensIndex - 2] + remainingStr
-        return tensStr
-    }
+		tensStr := tens[tensIndex-2] + remainingStr
+		return tensStr
+	}
 
-    if n < 1000 {
-        hundredsIndex := int(math.Floor(float64(n) / 100))
-        remaining := n - hundredsIndex * 100
-        hundredsStr := ""
-        
-        hundredsStr += below20[hundredsIndex - 1] + " hundred"
+	if n < 1000 {
+		hundredsIndex := int(math.Floor(float64(n) / 100))
+		remaining := n - hundredsIndex*100
+		hundredsStr := ""
 
-        if remaining > 0 {
-            hundredsStr += " and " + getWordForInt(remaining) 
-        }
-        
-        return hundredsStr
-    }
+		hundredsStr += below20[hundredsIndex-1] + " hundred"
 
-    if n < 10000 {
-        thousands := int(math.Floor(float64(n) / 1000))
-        remaining := n - thousands * 1000
-        
-        thousandsStr := getWordForInt(thousands) + " thousand"
+		if remaining > 0 {
+			hundredsStr += " and " + getWordForInt(remaining)
+		}
 
-        if remaining > 0 {
-            if remaining < 100 {
-                thousandsStr += " and " 
-            } else {
-                thousandsStr += ", "
-            }
-            thousandsStr += getWordForInt(remaining) 
-        }
+		return hundredsStr
+	}
 
-        return thousandsStr
-    }
+	if n < 10000 {
+		thousands := int(math.Floor(float64(n) / 1000))
+		remaining := n - thousands*1000
 
-    return ""
+		thousandsStr := getWordForInt(thousands) + " thousand"
+
+		if remaining > 0 {
+			if remaining < 100 {
+				thousandsStr += " and "
+			} else {
+				thousandsStr += ", "
+			}
+			thousandsStr += getWordForInt(remaining)
+		}
+
+		return thousandsStr
+	}
+
+	return ""
 }
-
